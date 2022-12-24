3f8b80e4f13f8fc242e0ac59a0e13b89bf2cc32c
Lotharie <49253492+LotharieSlayer@users.noreply.github.com>
2022-12-24 17:28:07 +0100

    Warn plugin transition

---
+++ utils/enmapUtils.js
@@ -0,0 +1,64 @@
+/**
+ * @author Lothaire Guée
+ * @description
+ *      Contains the function linked to the database.
+ */
+
+
+/* ----------------------------------------------- */
+/* DATABASES INITILIZATION                         */
+/* ----------------------------------------------- */
+const Enmap = require("enmap");
+
+// WARNS
+const warnedUsers = new Enmap({name: "warned_users"});
+const counter = new Enmap({name: "counter"});
+
+/* ----------------------------------------------- */
+/* FUNCTIONS                                       */
+/* ----------------------------------------------- */
+
+/**
+ * Commentaires
+ * @returns {String} Channel ID by passing the Guild ID and the type of
+ * the channel you want to search.
+ * Example : getSetupData(GUILD_ID, "presentation") but it can be : "proposition" or "discussion"
+ */
+async function getSetupData(id, type){
+
+    switch (type) {
+        case "warn_user":
+            // Here id is the member
+            return await getResultsValue(warnedUsers, id)
+        default:
+            break;
+    }
+
+}
+
+async function getResultsKey(db, id){
+    let result;
+    db.fetchEverything()?.forEach( async (value, key) => {
+        if(key === id)
+            result = key;
+    })
+    return result;
+}
+
+async function getResultsValue(db, id){
+    let result;
+    db.fetchEverything()?.forEach( async (value, key) => {
+        if(key === id)
+            result = value;
+    })
+    return result;
+}
+
+/* ----------------------------------------------- */
+/* MODULE EXPORTS                                  */
+/* ----------------------------------------------- */
+module.exports = {
+	getSetupData,
+    warnedUsers,
+    counter,
+}
